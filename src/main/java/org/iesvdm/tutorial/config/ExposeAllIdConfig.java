5e9d3785f9e6f430a3f6109e90585cba339a23cc
toteabe <toteabe@yahoo.es>
2024-02-11 20:01:26 +0100

    spring data rest - restcontroller - hal - spring doc open-api

---
+++ src/main/java/org/iesvdm/tutorial/config/ExposeAllIdConfig.java
@@ -0,0 +1,24 @@
+package org.iesvdm.tutorial.config;
+
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+/**
+ * CONFIG PARA EXPONER LOS IDs EN SPRING DATA REST
+ * ACCESIBLE EN /data-api
+ */
+@Configuration
+public class ExposeAllIdConfig implements RepositoryRestConfigurer {
+
+    @Autowired
+    private EntityManager entityManager;
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        config.exposeIdsFor(entityManager.getMetamodel().getEntities().stream().map(jakarta.persistence.metamodel.Type::getJavaType).toArray(Class[]::new));
+    }
+}
