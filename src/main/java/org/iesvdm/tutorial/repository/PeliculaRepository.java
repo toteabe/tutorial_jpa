1119aaffd9b347e410f5e9e9b244f75406ae974d
toteabe <toteabe@yahoo.es>
2024-02-21 11:23:26 +0100

    Añadiendo entityManager.refresh y method queries

--- src/main/java/org/iesvdm/tutorial/repository/PeliculaRepository.java
+++ src/main/java/org/iesvdm/tutorial/repository/PeliculaRepository.java
@@ -4,6 +4,13 @@ import org.iesvdm.tutorial.domain.Pelicula;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PeliculaRepository extends JpaRepository<Pelicula, Long> {
+
+    public List<Pelicula> findByTituloContainingIgnoreCaseOrderByTituloAsc(String titulo);
+
+    public List<Pelicula> findByTituloContainingIgnoreCaseOrderByTituloDesc(String titulo);
+
 }
--- src/main/java/org/iesvdm/tutorial/service/PeliculaService.java
+++ src/main/java/org/iesvdm/tutorial/service/PeliculaService.java
@@ -1,10 +1,16 @@
 package org.iesvdm.tutorial.service;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
 import org.iesvdm.tutorial.domain.Pelicula;
+import org.iesvdm.tutorial.exception.EntityNotFoundException;
 import org.iesvdm.tutorial.exception.PeliculaNotFoundException;
 import org.iesvdm.tutorial.repository.PeliculaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import java.util.List;
 
@@ -14,12 +20,23 @@ public class PeliculaService {
     @Autowired
     PeliculaRepository peliculaRepository;
 
+    @PersistenceContext
+    EntityManager entityManager;
+
+
     public List<Pelicula> all() {
         return this.peliculaRepository.findAll();
     }
 
+
+    @Transactional
     public Pelicula save(Pelicula pelicula) {
-        return this.peliculaRepository.save(pelicula);
+
+        this.peliculaRepository.save(pelicula);
+
+        this.entityManager.refresh(pelicula);
+
+        return pelicula;
     }
 
     public Pelicula one(Long id) {
